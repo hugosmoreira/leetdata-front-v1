a1f6167ca7216438b24974efa97ac7a07c767358
hugosmoreira <hugo.n4@gmail.com>
2022-05-13 22:52:08 -0700

    updapte pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export { default as Blog } from './containers/blog/Blog';
+export { default as Features } from './containers/features/Features';
+export { default as Footer } from './containers/footer/Footer';
+export { default as Header } from './containers/header/Header';
+export { default as Possibility } from './containers/possibility/Possibility';
+export { default as WhatGPT3 } from './containers/whatGPT3/WhatGPT3';
